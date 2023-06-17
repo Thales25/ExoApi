bbd7828526cbe9203a3d08847f5cd3d1e050698f
Thales25 <thalessousa8579@gmail.com>
2023-06-17 07:04:27 -0300

    implementando as operações basicas com o banco de dados, incluindo a opção de busca, cadastro e delete

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -19,7 +19,45 @@ namespace Exo.WebApi.Controllers
         [HttpGet]
         public IActionResult Listar()
         {
-            return Ok(_projetoRepository.Listar());
+            return Ok( _projetoRepository.Listar());
         }
+        // Código novo que completa o CRUD.
+        [HttpPost]
+        public IActionResult Cadastrar(projetos projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            projetos projeto = _projetoRepository.BuscarporId(id);
+            if (projeto == null)
+            {
+                return NotFound();
+            }
+                return Ok(projeto);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, projetos projeto)
+        {
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+                catch (Exception )
+            {
+            return BadRequest();
+            }
+        }
+
+
     }
 }
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -18,5 +18,35 @@ namespace Exo.WebApi.Repositores
         {
             return _context.projetos.ToList();
         }
-    }
+
+        //codigo novo que compila o CRUD
+        public void Cadastrar(projetos projeto)
+            {
+                 _context.projetos.Add(projeto);
+                 _context.SaveChanges();
+            }
+        public projetos BuscarporId(int id)
+            {
+                return _context.projetos.Find(id);
+            }
+            public void Atualizar(int id, projetos projeto)
+                {
+                    projetos projetoBuscado = _context.projetos.Find(id);
+                    if (projetoBuscado != null)
+                {
+                    projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+                    projetoBuscado.Area = projeto.Area;
+                    projetoBuscado.Status = projeto.Status;
+                }
+                    _context.projetos.Update(projetoBuscado);
+                    _context.SaveChanges();
+                }
+            public void Deletar(int id)
+                {
+                    projetos projetoBuscado = _context.projetos.Find(id);
+                    _context.projetos.Remove(projetoBuscado);
+                    _context.SaveChanges();
+                }
+
+    }   
 }
